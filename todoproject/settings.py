946f0bc14f82a5424a31bab814d364f04ec32466
gobadebr <gobadebr@gmail.com>
2021-06-07 08:31:17 +0530

    drf

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_DIRS = BASE_DIR/'static'
-STATIC_ROOT  =  BASE_DIR/'static'
+STATICFILES_DIRS = BASE_DIR/'staticfiles'
+STATIC_ROOT  =  [STATICFILES_DIRS]
 STATIC_URL = '/static/'
 
 # Default primary key field type
